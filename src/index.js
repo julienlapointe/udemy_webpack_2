07f99a72ad929fb3bf14d86f4030b946e0a00b79
Julien LaPointe <email@julienlapointe.com>
2017-07-24 17:09:23 -0500

    calculated sum() of 2 numbers in index.js + added .gitignore and webpack.config.js files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // sum.js must be loaded before index.js can be executed
 // index.js must import / require sum.js
 // './sum' represents a relative file path / reference (relative to the location of index.js)
-const sum = require('./sum');
+const sum = require("./sum");
 
 // add numbers together using the imported sum() function and print the sum "total" to the console
 const total = sum(10, 5);
