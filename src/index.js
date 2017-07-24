974abdaf6f04c34147e70fdf6cc11775ca5f76fa
Julien LaPointe <email@julienlapointe.com>
2017-07-24 14:33:39 -0500

    added + exported sum() function in sum.js and imported sum() function into index.js using the *Common.js module system*

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// learning #1
+// depends on sum.js ot execute
+// sum.js must be loaded before index.js can be executed
+// index.js must import / require sum.js
+// './sum' represents a relative file path / reference (relative to the location of index.js)
+const sum = require('./sum');
+
+// add numbers together using the imported sum() function and print the sum "total" to the console
+const total = sum(10, 5);
+console.log(total);
---
+++ src/sum.js
@@ -0,0 +1,9 @@
+// learning #2
+// each JS module has its own / separate scope
+// therefore, index.js cannot access the sum() function below... unless we "export" it
+
+// function that adds two numbers
+const sum = (a, b) => a + b;
+
+// make the sum() function public / available for other JS modules to use
+module.exports = sum;
