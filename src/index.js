3c2aa9c8576848671790137566ca3f3db4f6134a
Julien LaPointe <email@julienlapointe.com>
2017-07-24 22:07:43 -0500

    added comments re: CommonJS module system syntax for import of sum.js into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 // sum.js must be loaded before index.js can be executed
 // index.js must import / require sum.js
 // './sum' represents a relative file path / reference (relative to the location of index.js)
+// CommonJS module export syntax
 const sum = require("./sum");
 
 // add numbers together using the imported sum() function and print the sum "total" to the console
--- src/sum.js
+++ src/sum.js
@@ -6,4 +6,5 @@
 const sum = (a, b) => a + b;
 
 // make the sum() function public / available for other JS modules to use
+// CommonJS module export syntax
 module.exports = sum;
